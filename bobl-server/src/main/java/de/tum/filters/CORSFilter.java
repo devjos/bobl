0c0c752e52553cabc63ceab27a76454093b5ed54
Johannes Schleger <johannes.schleger@tum.de>
2017-07-17 10:36:09 +0200

    add bobl header

--- bobl-server/src/main/java/de/tum/filters/CORSFilter.java
+++ bobl-server/src/main/java/de/tum/filters/CORSFilter.java
@@ -17,7 +17,8 @@ public class CORSFilter implements ContainerResponseFilter {
   public void filter(ContainerRequestContext request, ContainerResponseContext response)
       throws IOException {
     response.getHeaders().add("Access-Control-Allow-Origin", "*");
-    response.getHeaders().add("Access-Control-Allow-Headers", "*");
+    response.getHeaders().add("Access-Control-Allow-Headers",
+        "origin, content-type, accept, authorization, bobl-cookie");
     response.getHeaders().add("Access-Control-Allow-Credentials", "true");
     response.getHeaders().add("Access-Control-Allow-Methods",
         "GET, POST, PUT, DELETE, OPTIONS, HEAD");
